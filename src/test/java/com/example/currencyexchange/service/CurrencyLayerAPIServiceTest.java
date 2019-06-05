aeed4b095b52da958f491495b6b10e41b554f098
DESKTOP-QJ641A4\count <counterchord89@gmail.com>
2019-06-05 14:02:57 +0900

    CurrencyLayerApiService 구현 및 환율정보 Test 작성

---
+++ src/test/java/com/example/currencyexchange/service/CurrencyLayerAPIServiceTest.java
@@ -0,0 +1,32 @@
+package com.example.currencyexchange.service;
+
+import com.example.currencyexchange.dto.CurrencyLayerDto;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Mono;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyLayerAPIServiceTest {
+
+    @Autowired
+    CurrencyLayerAPIService currencyLayerAPIService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyLayerAPIService.class);
+
+    @Test
+    public void 환율정보_불러오기(){
+        Mono<CurrencyLayerDto> searchResult = currencyLayerAPIService.getCurrencyRate();
+        Assert.assertNotNull(searchResult);
+        searchResult.doOnNext(r->{
+            System.out.println("result: " + r.getQuotes());
+        } ).subscribe();
+        LOGGER.info("test result: {}", searchResult.block());
+    }
+
+}
