d3b252c6037741789b4269a9e189cc7ac2912210
DESKTOP-QJ641A4\count <counterchord89@gmail.com>
2019-06-08 14:06:34 +0900

    Revert "부트스트랩 적용 및 WebConfig 수정"
    This reverts commit 575044480b1eda565c013798dfd99bc1d51be1a2.

--- src/main/java/com/example/currencyexchange/scheduler/GetCurrencyRateScheduler.java
+++ src/main/java/com/example/currencyexchange/scheduler/GetCurrencyRateScheduler.java
@@ -3,6 +3,7 @@ package com.example.currencyexchange.scheduler;
 import com.example.currencyexchange.service.CurrencyLayerAPIService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
